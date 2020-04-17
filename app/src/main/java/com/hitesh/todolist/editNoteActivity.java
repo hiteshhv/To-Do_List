7d7d95fa52c2b6761929546bf56ac4897c694b15
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-04-17 08:28:36 +0530

    Initial Push

---
+++ app/src/main/java/com/hitesh/todolist/editNoteActivity.java
@@ -0,0 +1,56 @@
+package com.hitesh.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+public class editNoteActivity extends AppCompatActivity {
+
+    EditText saveNote;
+    int noteId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+
+        saveNote = (EditText) findViewById(R.id.saveNote);
+
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId", -1);
+
+        if(noteId != -1) {
+            saveNote.setText(MainActivity.notes.get(noteId));
+        } else {
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() -1;
+        }
+        saveNote.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            MainActivity.notes.set(noteId,String.valueOf(s));
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.hitesh.todolist", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
