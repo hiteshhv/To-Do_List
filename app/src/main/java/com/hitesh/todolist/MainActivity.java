dd62aa29118c3053e6dff90fb3ef080785c0dabf
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-04-17 17:18:24 +0530

    Fix Build

--- app/src/main/java/com/hitesh/todolist/MainActivity.java
+++ app/src/main/java/com/hitesh/todolist/MainActivity.java
@@ -1,10 +1,7 @@
 package com.hitesh.todolist;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -21,32 +18,32 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
     static ArrayList<String> notes = new ArrayList<>();
-    static ArrayAdapter<String> arrayAdapter;
+    static ArrayAdapter arrayAdapter;
     SharedPreferences sharedPreferences;
 
-    @SuppressLint("Assert")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView =(ListView) findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
-        listView.setAdapter(arrayAdapter);
-
         sharedPreferences = getApplicationContext().getSharedPreferences("com.hitesh.todolist", Context.MODE_PRIVATE);
-        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes" , null);
+        ListView listView = findViewById(R.id.listView);
 
-        if (notes.isEmpty()){
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
 
+        if (set == null) {
             notes.add("Example Note");
         } else {
             notes = new ArrayList(set);
         }
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -74,22 +71,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.top_menu, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
 
-        if(item.getItemId() == R.id.add_note){
+        if (item.getItemId() == R.id.add_note) {
             Intent intent = new Intent(getApplicationContext(), editNoteActivity.class);
             startActivity(intent);
+
             return true;
         }
-        return super.onOptionsItemSelected(item);
+
+        return false;
     }
 }
